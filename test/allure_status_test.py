a2ea4f7bd8dfcd85b5f3736066f7dd2a3b39bc8c
Dzipt <dzipt2@mail.ru>
2026-06-05 12:10:42 +0300

    Added issues from Jira

--- test/allure_status_test.py
+++ test/allure_status_test.py
@@ -6,6 +6,8 @@ from allure_commons.types import Severity
 @allure.epic("Allure TestOps")
 @allure.tag("smokeT", "check")
 @allure.severity(allure.severity_level.CRITICAL)
+@allure.issue("KAN", "Test in Code")
+@allure.link("https://dzipt.atlassian.net/jira/software/projects/KAN/boards/2?selectedIssue=KAN-4", link_type=allure.link_type.ISSUE)
 def test_passed():
     pass
 
